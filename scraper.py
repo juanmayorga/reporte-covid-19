9f182cbe6bd9deba43fcbec9b148f80f79f213a6
juanmayorga <juanxmayorga@gmail.com>
2021-08-10 15:06:54 -0400

    seteo de datos por comuna elegida

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-#from openpyxl.workbook import Workbook
 import requests
 import datetime
 import os
 from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import numbers
 import lxml.html as html
 
 
@@ -19,21 +20,29 @@ def home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             comuna = input('Ingresa comuna: ')
-            comuna = '//tr[contains(.,"' + comuna + '")]/td/text()'
-            # print(comuna)
+            comunaxpath = '//tr[contains(.,"' + comuna + '")]/td/text()'
             headers = parsed.xpath(XPATH_HEADERS)
-            data = parsed.xpath(comuna)
+            data = parsed.xpath(comunaxpath)
             today = datetime.date.today().strftime('%d-%m-%Y')
             wb = Workbook()
             ws = wb.active
             ws.title = "reporte " + today
-            # for header in headers:
-            ws.append(headers)
-            wb.save(f'reporte {today}.xlsx')
-            # if not os.path.isdir(f'reporte ', today):
-            #   os.mkdir(today)
+            for i in range(0, 5):
+                _ = ws.cell(column=i+1, row=1, value=headers[i])
+                _ = ws.cell(column=i+1, row=2, value=data[i])
 
-            #parse_news(urlhome+link, today)
+            _ = ws.cell(column=1, row=4, value="Fecha")
+            _ = ws.cell(column=1, row=5, value="Casos Totales")
+            _ = ws.cell(column=1, row=6, value="Casos Diarios")
+            for i in range(5, len(data)):
+                _ = ws.cell(column=i-3, row=4, value=headers[i])
+                _ = ws.cell(column=i-3, row=5, value=int(float(data[i])))
+            #_ = ws.cell(column=2, row=6, value='=SUM(B5:C5)')
+
+            for i in range(1, len(headers)):
+                ws.column_dimensions[get_column_letter(i)].auto_size = True
+
+            wb.save(f'Reporte {comuna} {today}.xlsx')
 
         else:
             raise ValueError(f'Error: {response.status_code}')
